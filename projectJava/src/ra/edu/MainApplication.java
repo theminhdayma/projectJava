5a1c1c0d0142b8f24b8ee9546c99852d3a8af4e2
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2025-04-16 04:56:02 +0700

    Add task 2

---
+++ projectJava/src/ra/edu/MainApplication.java
@@ -0,0 +1,7 @@
+package ra.edu;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ projectJava/src/ra/edu/business/config/ConnectionDB.java
@@ -0,0 +1,44 @@
+package ra.edu.business.config;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/...";
+    private static final String USER = "theminhdayma";
+    private static final String PASSWORD = "123456";
+
+    public static Connection openConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Lỗi kết nối CSDL do " + e.getMessage());
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn, CallableStatement callSt) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (callSt != null) {
+            try {
+                callSt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
